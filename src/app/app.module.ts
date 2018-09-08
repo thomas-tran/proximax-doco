508e856a3ac9cba7b86e4b2978b8b63c930b8f81
Thomas Tran <tldinteractive@gmail.com>
2018-09-08 21:45:15 +1000

    test xpx-angular-js

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,13 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { environment } from '../environments/environment';
+import {
+  PROXIMAX_REMOTE_BASE_URL,
+  NEM_NETWORK
+} from 'xpx-typescript-angular-sdk';
+import { NetworkTypes } from 'nem-library';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [AppComponent],
@@ -15,12 +22,18 @@ import { AppRoutingModule } from './app-routing.module';
   imports: [
     BrowserModule,
     IonicModule.forRoot(),
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [
     StatusBar,
     SplashScreen,
-    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+    {
+      provide: PROXIMAX_REMOTE_BASE_URL,
+      useValue: environment.ProximaX.remoteConnection
+    },
+    { provide: NEM_NETWORK, useValue: NetworkTypes.TEST_NET }
   ],
   bootstrap: [AppComponent]
 })
--- src/app/list/list.page.ts
+++ src/app/list/list.page.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import {
+  RemoteUploadService,
+  UploadTextRequest,
+  ResourceHashMessage,
+  MessageType
+} from 'xpx-typescript-angular-sdk';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-list',
@@ -6,32 +13,75 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['list.page.scss']
 })
 export class ListPage implements OnInit {
-  private selectedItem: any;
-  private icons = [
-    'flask',
-    'wifi',
-    'beer',
-    'football',
-    'basketball',
-    'paper-plane',
-    'american-football',
-    'boat',
-    'bluetooth',
-    'build'
-  ];
-  public items: Array<{ title: string; note: string; icon: string }> = [];
-  constructor() {
-    for (let i = 1; i < 11; i++) {
-      this.items.push({
-        title: 'Item ' + i,
-        note: 'This is item #' + i,
-        icon: this.icons[Math.floor(Math.random() * this.icons.length)]
-      });
-    }
-  }
+  uploadForm: FormGroup;
+  uploaded = false;
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private uploadService: RemoteUploadService
+  ) {}
 
   ngOnInit() {
+    this.uploadForm = this.formBuilder.group({
+      title: ['', Validators.required],
+      message: ['', Validators.required],
+      keywords: [''],
+      metadata: [''],
+      uploadResult: ['']
+    });
   }
+
+  get f() {
+    return this.uploadForm.controls;
+  }
+
+  onUpload() {
+    this.uploaded = true;
+
+    // stop here if form is invalid
+    if (this.uploadForm.invalid) {
+      return;
+    }
+
+    const title = this.f.title.value;
+    const contentType = 'text/plain';
+    const encoding = 'utf-8';
+    const message = this.f.message.value;
+    let keywords = '';
+    let metadata = '';
+
+    if (this.f.metadata.value.length > 0) {
+      metadata = JSON.stringify(this.f.metadata.value);
+    }
+
+    if (this.f.keywords.value.length > 0) {
+      keywords = this.f.keywords.value;
+    }
+
+    const payload: UploadTextRequest = {
+      name: title,
+      contentType: contentType,
+      encoding: encoding,
+      keywords: keywords,
+      metadata: metadata,
+      text: message,
+      messageType: MessageType.PLAIN,
+      senderPrivateKey: '',
+      recieverPublicKey: ''
+    };
+
+    this.uploadService.uploadText(payload).subscribe(
+      result => {
+         const rhm: ResourceHashMessage = result;
+        console.log(rhm);
+        this.f.uploadResult.setValue(rhm.hash());
+      },
+      error => {
+        console.log(error);
+      }
+    );
+  }
+
   // add back when alpha.4 is out
   // navigate(item) {
   //   this.router.navigate(['/list', JSON.stringify(item)]);
